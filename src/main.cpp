675d23c4d61a3792c3544aa85729d71026a76984
woshiyanghang <yh_swjtu@163.com>
2020-09-26 19:06:32 +0800

    增加了一个语法分析小例子

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,16 @@ extern "C" {
      * 带词汇表的词法分析工具
     */
     void lex_parser_with_word_list();
+    /**
+     * 语法分析功能的工具
+    */
+    void grammar_parse();
 }
 
 int main(int argc, char ** args) {
     cout << "Hello, world" << endl;
-    lex_parser_with_word_list();
+    cout << argc << endl;
+    char **fileList = args + 1;
+    cout << args[0] << endl;
     return 0;
 }

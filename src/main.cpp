0567326d2747925dc5d985cb0b4d7a2c5e44b070
woshiyanghang <yh_swjtu@163.com>
2021-08-29 20:28:57 +0800

    <refactor> add code about using lex

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,24 @@ extern "C" {
     /**
      * 带词汇表的词法分析工具
     */
-    void lex_parser_with_symbol_table();
+   void lex_parser_with_symbol_table();
     /**
      * 带有词法分析器的语法分析工具
     */
    void grammar_parse();
+   /**
+    * 字符数统计工具
+    * 
+    * @param int 接受命令行第一个参数，用于指示有多少行
+    * @param char 接受命令行参数中具体的参数
+   */
+  void word_count_func(int, char**);
+  int simple_word_count(int, char**);
+  void parse_command_line();
 }
 
 int main(int argc, char ** args) {
     cout << "Begin..." << endl;
-    grammar_parse();
+    parse_command_line();
     return 0;
 }
